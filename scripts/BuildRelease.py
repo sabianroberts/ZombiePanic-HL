33ea96ba3139c37a3f79849a02ba0f8ad7bdcfc9
Johan Ehrendahl <jonnyboy0719@gmail.com>
2025-05-25 11:09:15 +0200

    updated BuildRelease.py

--- scripts/BuildRelease.py
+++ scripts/BuildRelease.py
@@ -13,7 +13,7 @@ from CreateMetadata import create_metadata
 
 
 # Also needs to be changed in CMakeLists.txt
-DEFAULT_VERSION = [1, 13, 0, 'dev', '']
+DEFAULT_VERSION = [1, 1, 0, 'pre-release', '']
 
 
 # ---------------------------------------------
