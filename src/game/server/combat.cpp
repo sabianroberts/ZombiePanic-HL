292962ad1ae76b386ad2e5ed96696c512f21423a
Johan Ehrendahl <jonnyboy0719@gmail.com>
2025-07-13 15:47:47 +0200

    Reduced ground knockback by clamping the maximum force and removed air knockback

--- src/game/server/combat.cpp
+++ src/game/server/combat.cpp
@@ -870,9 +870,15 @@ int CBaseMonster ::TakeDamage(entvars_t *pevInflictor, entvars_t *pevAttacker, f
 	}
 
 	// if this is a player, move him around!
-	if ((!FNullEnt(pevInflictor)) && (pev->movetype == MOVETYPE_WALK) && (!pevAttacker || pevAttacker->solid != SOLID_TRIGGER))
+	// ZP Edit: Make sure it's only on the ground!!
+	if ((pev->flags & FL_ONGROUND) || pev->groundentity)
 	{
-		pev->velocity = pev->velocity + vecDir * -DamageForce(flDamage);
+		if ((!FNullEnt(pevInflictor)) && (pev->movetype == MOVETYPE_WALK) && (!pevAttacker || pevAttacker->solid != SOLID_TRIGGER))
+		{
+			// Clamp it, for balance reasons.
+			float flDmgClamp = clamp(flDamage, 0, 25);
+			pev->velocity = pev->velocity + vecDir * -DamageForce(flDmgClamp);
+		}
 	}
 
 	// do the damage
